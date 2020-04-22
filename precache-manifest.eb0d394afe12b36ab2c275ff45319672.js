8ce3526102815a65ae5db79d0a7ba09d6c445613
Theacalix <Theacalix@users.noreply.github.com>
2020-04-22 02:17:14 -0700

    Updates

---
+++ precache-manifest.eb0d394afe12b36ab2c275ff45319672.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d396e344412230e02fe2e275dbbd5e9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "60b1fdc5821655470785",
+    "url": "/portfolio/static/css/main.c61c1056.chunk.css"
+  },
+  {
+    "revision": "3f89ee857f0358074a6c",
+    "url": "/portfolio/static/js/2.71819bae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.71819bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60b1fdc5821655470785",
+    "url": "/portfolio/static/js/main.a9c43313.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  },
+  {
+    "revision": "85340738d890b77822d0096b64659643",
+    "url": "/portfolio/static/media/AddHabit.85340738.gif"
+  },
+  {
+    "revision": "8fae379b7833efcbdccaece1df149abe",
+    "url": "/portfolio/static/media/AddTask.8fae379b.gif"
+  },
+  {
+    "revision": "264a1142bd2fe96cff0a487ff691ee92",
+    "url": "/portfolio/static/media/CompleteHabit.264a1142.gif"
+  },
+  {
+    "revision": "62c7d7ae3c7766a55e4a9736fd9e4925",
+    "url": "/portfolio/static/media/altLogos.62c7d7ae.png"
+  },
+  {
+    "revision": "73fe2cb94df8cc1c608a41c7cecb4749",
+    "url": "/portfolio/static/media/clickTile.73fe2cb9.PNG"
+  },
+  {
+    "revision": "ac8124e979d0e34750ecc4a334b96234",
+    "url": "/portfolio/static/media/conferenceDemo.ac8124e9.JPG"
+  },
+  {
+    "revision": "3efde84a19897145faeacb01bf8dc2ba",
+    "url": "/portfolio/static/media/hardwareLayout.3efde84a.jpg"
+  },
+  {
+    "revision": "3f9978681f4c707c3a46682dbe1e9521",
+    "url": "/portfolio/static/media/heritageFarmed.3f997868.png"
+  },
+  {
+    "revision": "9441ed483763595568709cda15c7aa1f",
+    "url": "/portfolio/static/media/heritageFarmedBrandStandards.9441ed48.pdf"
+  },
+  {
+    "revision": "c5277cdaa4bab3096d5513d435575283",
+    "url": "/portfolio/static/media/logo.c5277cda.png"
+  },
+  {
+    "revision": "09ae1fb6c07336ddd47fd5b8b2de1ea0",
+    "url": "/portfolio/static/media/mapmakerDisplay.09ae1fb6.png"
+  },
+  {
+    "revision": "f479aae5c999ae73c1f3f30a8780e40b",
+    "url": "/portfolio/static/media/mapmakerEdit.f479aae5.png"
+  },
+  {
+    "revision": "9e1443021c3506e68a89183750654792",
+    "url": "/portfolio/static/media/otherLogo.9e144302.png"
+  },
+  {
+    "revision": "026f93878cee66240232f2fcbfb12957",
+    "url": "/portfolio/static/media/packaging.026f9387.png"
+  },
+  {
+    "revision": "62760cf35f0a134f9888d30365933c5b",
+    "url": "/portfolio/static/media/profile.62760cf3.JPG"
+  },
+  {
+    "revision": "c84e20a012b3ede58a485106eff6a0de",
+    "url": "/portfolio/static/media/smallLogo.c84e20a0.png"
+  },
+  {
+    "revision": "dfc9eef88086fc91425dbae08cc887a9",
+    "url": "/portfolio/static/media/taskPage.dfc9eef8.png"
+  }
+]);
