3e0bf927501dedf37557079ddac1b944fed4a396
Theacalix <Theacalix@users.noreply.github.com>
2020-05-07 12:20:44 -0700

    added ui elements

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faCircle,
-  faArrowDown,
+  faArrowRight,
   faArrowUp,
   faHome,
   faMap,
   faSpa,
   faClipboardCheck,
-  faUser
+  faUser,
+  faTimes,
+  faExpandAlt
  } from '@fortawesome/free-solid-svg-icons';
 import {
   Link,
@@ -40,20 +42,32 @@ import completeHabit from './CompleteHabit.gif';
 
 
 library.add(faCircle,
-  faArrowDown,
+  faArrowRight,
   faArrowUp,
   faHome,
   faMap,
   faSpa,
   faClipboardCheck,
-  faUser);
+  faUser,
+  faTimes,
+  faExpandAlt);
 
   const icons = {
-    "intro": "home",
-    "map": "map",
-    "brand": "spa",
-    "todo": "clipboard-check",
-    "about": "user"
+    "intro": {
+      icon: "home",
+      label: "Intro"},
+    "map": {
+      icon: "map",
+      label: "Map Maker"},
+    "brand": {
+      icon: "spa",
+      label: "Heritage Farmed"},
+    "todo": {
+      icon: "clipboard-check",
+      label: "AchieveIt"},
+    "about": {
+      icon: "user",
+      label: "About"}
   }
 
   const backgrounds = ["#e7e6f3", "#afa7d2", "#968abf"]
@@ -74,35 +88,95 @@ class NavElem extends React.Component {
     this.setState({selected: true});
   }
   handleLeave() {
-    this.setState({selected: this.state.active});
+    this.setState({selected: false});
   }
   handleActive() {
-    this.setState({selected: true, active: true});
+    this.setState({active: true});
   }
   handleInactive() {
-    this.setState({selected: false, active: false});
+    this.setState({active: false});
   }
-  selectIcon() {
-    if(!this.state.selected) {
-      return "circle";
+  // selectIcon() {
+  //   if(!this.state.selected) {
+  //     return "circle";
+  //   }
+  //   else {
+  //     return icons[this.props.name].icon;
+  //   }
+  // }
+  render () {
+    let elem;
+    if(this.state.selected) {
+      elem = <div className="label" onMouseEnter={this.handleEnter} onMouseLeave={this.handleLeave}>
+              <FontAwesomeIcon
+              icon={icons[this.props.name].icon} size="lg"/>
+              <span>
+                {icons[this.props.name].label}
+              </span>
+            </div>
+    } else if (this.state.active) {
+      elem = <div className="icon">
+              <FontAwesomeIcon
+              onMouseEnter={this.handleEnter}
+              onMouseLeave={this.handleLeave}
+              icon={icons[this.props.name].icon} size="lg"/>
+            </div>
     }
     else {
-      return icons[this.props.name];
+      elem = <div className="icon">
+              <FontAwesomeIcon
+              onMouseEnter={this.handleEnter}
+              onMouseLeave={this.handleLeave}
+              icon="circle" size="xs"/>
+            </div>
     }
-  }
-  render () {
     return (
       <Link to={this.props.name} spy={true} smooth={true} duration={500} onSetActive={this.handleActive} onSetInactive={this.handleInactive}>
-        <FontAwesomeIcon
-        onMouseEnter={this.handleEnter}
-        onMouseLeave={this.handleLeave}
-        icon={this.selectIcon()} size={this.state.selected ? "lg" : "xs"}/>
+        {elem}
       </Link>
     ); //TODO: func to select icon
   }
 }
 
+class ImgExpansion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: false, active: false};
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleLeave = this.handleLeave.bind(this);
+    this.open = this.open.bind(this);
+    this.close = this.close.bind(this);
+  }
+  handleEnter() {
+    this.setState({active: true});
+  }
+  handleLeave() {
+    this.setState({active: false});
+  }
+  open() {
+    this.setState({open: true});
+  }
+  close() {
+    this.setState({open: false});
+  }
+  render() {
+    return (<>
+      <div className="imgWrapper" onMouseEnter={this.handleEnter} onMouseLeave={this.handleLeave}>
+        {this.state.active &&
+        <FontAwesomeIcon onClick={this.open} icon="expand-alt" size="lg" className={this.props.darkImg ? "light" : "dark"}/>}
+        <img src={this.props.src} alt={this.props.alt}/>
+      </div>
+      {this.state.open &&
+      <div className="imgPopup">
+        <FontAwesomeIcon onClick={this.close} icon="times"/>
+        <img src={this.props.src} alt={this.props.alt}/>
+      </div>}
+    </>);
+  }
+}
+
 class SideImg extends React.Component {
+
   render() {
     return (
       <div className={'sideBySide ' + this.props.side}>
@@ -133,21 +207,98 @@ class ImgRow extends React.Component {
   }
 }
 
-class Content extends React.Component {
-  componentDidMount() {
-    animateScroll.scrollTo(document.querySelector(`#${this.props.parentName}Content`).offsetTop - 100, {duration: 500, smooth: true});
+class TooltipBase extends React.Component {
+  constructor(props) {
+    super(props);
+    this.tooltip = React.createRef();
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleLeave = this.handleLeave.bind(this);
+  }
+  handleEnter(evt) {
+    let el = evt.currentTarget;
+
+    if(el != null) {
+      let rect = el.getBoundingClientRect();
+      this.tooltip.current.show(rect);
+    }
+  }
+
+  handleLeave() {
+    this.tooltip.current.hide();
   }
   render() {
-    return (<Element
-            id= {this.props.parentName + 'Content'}
-            className="lower"
-          >
-            {moreContent[this.props.parentName]}
-            <FontAwesomeIcon
-              icon="arrow-up"
-              size="2x" onClick={this.props.onClick}
-            />
-          </Element>);
+    return (<>
+      <strong className="word" onMouseEnter={this.handleEnter} onMouseLeave={this.handleLeave}>{this.props.word}</strong>
+      <Tooltip ref={this.tooltip} def={this.props.def} word={this.props.word}/>
+    </>)
+  }
+}
+
+class Tooltip extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {visible:false, x:0, y:0, type:"none"};
+  }
+
+  render () {
+    let {state} = this;
+    let visiblity = state.visible == true ? "on" : "off";
+
+    let style = {
+      left: ((state.x + window.scrollX) + 'px'),
+      top: ((state.y + window.scrollY) + 'px')
+    };
+
+    let classNames = {tooltip: true};
+
+    if(state.type != null && state.type != "none") {
+      classNames[state.type] = true;
+    }
+
+    classNames[visiblity] = true;
+
+    return (
+      <div className={Object.keys(classNames).join(" ")} style={style}>
+        <div className="tooltip-arrow"></div>
+        <div className="tooltip-inner">
+        <h4>{this.props.word}: </h4>{this.props.def}</div>
+      </div>
+    );
+  }
+
+  pastShow(hoverRect) {
+    let ttNode = ReactDOM.findDOMNode(this);
+    if(ttNode != null) {
+      let x = 0, y = 0, newState = {};
+      const docWidth = document.documentElement.clientWidth,
+      docHeight = document.documentElement.clientHeight;
+
+      let rx = hoverRect.x + hoverRect.width,
+          lx = hoverRect.x,
+          ty = hoverRect.y,
+          by = hoverRect.y + hoverRect.height;
+
+      let ttRect = ttNode.getBoundingClientRect();
+
+      //top
+      y = ty - ttRect.height;
+      x = lx + ((hoverRect.width - ttRect.width) / 2);
+
+      if(x < 0) {
+        x = lx;
+      }
+      newState = {type:"top", x:x, y:y};
+      this.setState(newState);
+    }
+  }
+
+  show(hoverRect) {
+    let {pastShow} = this;
+    this.setState({visible:true}, pastShow.bind(this, hoverRect));
+  }
+  hide() {
+    this.setState({visible:false});
   }
 }
 
@@ -180,8 +331,8 @@ class ProjectPage extends React.Component {
 
   render() {
     let image, more;
-    if(this.props.imgUrl) {
-      image = <img src={this.props.imgUrl} alt="test" />;
+    if(this.props.img) {
+      image = <ImgExpansion src={this.props.img.src} alt={this.props.img.alt} darkImg={this.props.img.dark}/>;
     }
     if(this.props.showMore && this.state.showing) {
       more = <Content
@@ -196,11 +347,8 @@ class ProjectPage extends React.Component {
         <div className = "text">
           <h2>{this.props.title}</h2>
           <p>{this.props.intro}</p>
-          {(this.props.showMore && !this.state.showing) ? <FontAwesomeIcon
-            icon="arrow-down"
-            size="2x"
-            onClick={this.toggleContent}
-          /> : ''}
+          {(this.props.showMore && !this.state.showing) ? <a href="#" className="more" onClick={this.toggleContent}>Learn More <FontAwesomeIcon
+          icon="arrow-right"/></a> : ''}
         </div>
       </div>
       {more}
@@ -209,14 +357,29 @@ class ProjectPage extends React.Component {
   }
 }
 
+class Content extends React.Component {
+  componentDidMount() {
+    animateScroll.scrollTo(document.querySelector(`#${this.props.parentName}Content`).offsetTop - 100, {duration: 500, smooth: true});
+  }
+  render() {
+    return (<Element
+            id= {this.props.parentName + 'Content'}
+            className="lower"
+          >
+            {moreContent[this.props.parentName]}
+            <a href="#" className="more" onClick={this.props.onClick}>Close</a>
+          </Element>);
+  }
+}
+
 class Site extends React.Component {
   render() {
     return (<>
       <div className = "nav">
         <NavElem name="intro"/>
-        <NavElem name="map"/>
-        <NavElem name="brand"/>
         <NavElem name="todo"/>
+        <NavElem name="brand"/>
+        <NavElem name="map"/>
         <NavElem name="about"/>
       </div>
       <ProjectPage
@@ -228,31 +391,31 @@ class Site extends React.Component {
       />
       <ProjectPage
         i={1}
-        name='map'
-        imgUrl={clickTile}
-        title='Map Maker'
-        intro='MapMaker fills a need in the tabletop RPG community by being an easy to use map creation web app that takes advantage of modern technology to enhance in-person game play.'
-      />
-      <ProjectPage
-        i={2}
-        name='brand'
-        reverse={true}
-        imgUrl={heritageFarmed}
-        title='Heritage Farmed'
-        intro='Heritage Farmed creates handcrafted botanicals with an emphasis on the community, the environment, and ending the stigma against cannabis. They make medicinal healing products with an emphasis on local, sustainable ingredients. My job was to bring their vision to life and create a full brand material package.'
-      />
-      <ProjectPage
-        i={1}
         name='todo'
-        imgUrl={taskPage}
+        img={{src: taskPage, alt: "task screen"}}
         title='AchieveIt'
         intro='A productivity app designed for people with mental illness. Designed to be gentle, structured, and easy to use. This app can be a helpful tool for people with difficulty remembering tasks and executing them at the right time and place.'
       />
       <ProjectPage
         i={2}
+        name='brand'
+        reverse={true}
+        img={{src: heritageFarmed, alt: 'heritage farmed logo', dark: true}}
+        title='Heritage Farmed'
+        intro='Heritage Farmed creates handcrafted botanicals with an emphasis on the community, the environment, and ending the stigma against cannabis. They make medicinal healing products with an emphasis on local, sustainable ingredients. My job was to bring their vision to life and create a full brand material package.'
+      />
+      <ProjectPage
+        i={1}
+        name='map'
+        img={{src: clickTile, alt: 'map maker screen'}}
+        title='Map Maker'
+        intro='MapMaker fills a need in the tabletop RPG community by being an easy to use map creation web app that takes advantage of modern technology to enhance in-person game play.'
+      />
+      <ProjectPage
+        i={2}
         name='about'
         reverse={true}
-        imgUrl={profile}
+        img={{src: profile, alt: 'portrait photo'}}
         title="More About Me"
         intro='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
       />
@@ -335,7 +498,7 @@ let moreContent = {
       </>,
   todo: <>
     <p>
-      This project was designed to help people with executive functioning issues stemming from ADHD, Autism, or Anxiety/Depression achieve big and small goals. The three main features of the app are:
+      This project was designed to help people with <TooltipBase word='executive functioning' def='the mental processes that enable us to plan, focus attention, remember instructions, and juggle multiple tasks successfully.'/> issues stemming from ADHD, Autism, or Anxiety/Depression achieve big and small goals. The three main features of the app are:
     </p>
     <ul>
       <li>Provide custom reminders to help users complete tasks and create routines</li>
