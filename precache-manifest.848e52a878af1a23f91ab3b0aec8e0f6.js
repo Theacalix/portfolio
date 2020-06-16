bd0a75f99b088fe696d8fd30e31c3cf486d1c90e
Theacalix <Theacalix@users.noreply.github.com>
2020-06-16 16:37:26 -0700

    Updates

---
+++ precache-manifest.848e52a878af1a23f91ab3b0aec8e0f6.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b810316c4ef11da75c11e587a12fa3a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "120819067ba4e99f8b34",
+    "url": "/static/css/main.e492d975.chunk.css"
+  },
+  {
+    "revision": "b5c8aaf57763e0cd5da5",
+    "url": "/static/js/2.0bccaeeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0bccaeeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120819067ba4e99f8b34",
+    "url": "/static/js/main.fbc8acc6.chunk.js"
+  },
+  {
+    "revision": "4c15ceb5499f6abee85b",
+    "url": "/static/js/runtime-main.eb07c764.js"
+  },
+  {
+    "revision": "85340738d890b77822d0096b64659643",
+    "url": "/static/media/AddHabit.85340738.gif"
+  },
+  {
+    "revision": "8fae379b7833efcbdccaece1df149abe",
+    "url": "/static/media/AddTask.8fae379b.gif"
+  },
+  {
+    "revision": "264a1142bd2fe96cff0a487ff691ee92",
+    "url": "/static/media/CompleteHabit.264a1142.gif"
+  },
+  {
+    "revision": "73fe2cb94df8cc1c608a41c7cecb4749",
+    "url": "/static/media/clickTile.73fe2cb9.PNG"
+  },
+  {
+    "revision": "ac8124e979d0e34750ecc4a334b96234",
+    "url": "/static/media/conferenceDemo.ac8124e9.JPG"
+  },
+  {
+    "revision": "3efde84a19897145faeacb01bf8dc2ba",
+    "url": "/static/media/hardwareLayout.3efde84a.jpg"
+  },
+  {
+    "revision": "91def5aaa6376f789cfa1ec905cf6f74",
+    "url": "/static/media/headshot.91def5aa.jpg"
+  },
+  {
+    "revision": "3f9978681f4c707c3a46682dbe1e9521",
+    "url": "/static/media/heritageFarmed.3f997868.png"
+  },
+  {
+    "revision": "9441ed483763595568709cda15c7aa1f",
+    "url": "/static/media/heritageFarmedBrandStandards.9441ed48.pdf"
+  },
+  {
+    "revision": "c5277cdaa4bab3096d5513d435575283",
+    "url": "/static/media/logo.c5277cda.png"
+  },
+  {
+    "revision": "09ae1fb6c07336ddd47fd5b8b2de1ea0",
+    "url": "/static/media/mapmakerDisplay.09ae1fb6.png"
+  },
+  {
+    "revision": "f479aae5c999ae73c1f3f30a8780e40b",
+    "url": "/static/media/mapmakerEdit.f479aae5.png"
+  },
+  {
+    "revision": "9e1443021c3506e68a89183750654792",
+    "url": "/static/media/otherLogo.9e144302.png"
+  },
+  {
+    "revision": "026f93878cee66240232f2fcbfb12957",
+    "url": "/static/media/packaging.026f9387.png"
+  },
+  {
+    "revision": "b4eab27648466787eea174e1e8bc043e",
+    "url": "/static/media/portrait.b4eab276.gif"
+  },
+  {
+    "revision": "c84e20a012b3ede58a485106eff6a0de",
+    "url": "/static/media/smallLogo.c84e20a0.png"
+  },
+  {
+    "revision": "dfc9eef88086fc91425dbae08cc887a9",
+    "url": "/static/media/taskPage.dfc9eef8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3dba44b11f24a466bef8c0ca430eb4b4.js"
+  "/precache-manifest.848e52a878af1a23f91ab3b0aec8e0f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
