f10dd17e44bb02ff879e468f334aeefa72781c83
Theacalix <Theacalix@users.noreply.github.com>
2020-06-16 15:08:00 -0700

    added portrait and about me

--- src/index.js
+++ src/index.js
@@ -25,12 +25,12 @@ import './index.css';
 import clickTile from './clickTile.PNG';
 import heritageFarmed from './heritageFarmed.png';
 import taskPage from './taskPage.png';
-import profile from './profile.JPG';
+// import profile from './profile.JPG';
 import mapmakerEdit from './mapmakerEdit.png';
 import mapmakerDisplay from './mapmakerDisplay.png';
 import hardwareLayout from './hardwareLayout.jpg';
 import conferenceDemo from './conferenceDemo.JPG';
-import altLogos from './altLogos.png';
+// import altLogos from './altLogos.png';
 import logo from './logo.png';
 import otherLogo from './otherLogo.png';
 import smallLogo from './smallLogo.png';
@@ -39,6 +39,8 @@ import heritageFarmedBrandStandards from './heritageFarmedBrandStandards.pdf';
 import addTask from './AddTask.gif';
 import addHabit from './AddHabit.gif';
 import completeHabit from './CompleteHabit.gif';
+import headshot from './headshot.jpg';
+import portrait from './portrait.gif';
 
 
 library.add(faCircle,
@@ -55,22 +57,31 @@ library.add(faCircle,
   const icons = {
     "intro": {
       icon: "home",
-      label: "Intro"},
+      label: "Intro",
+      background: "#afa7d2"},
     "map": {
       icon: "map",
-      label: "Map Maker"},
+      label: "Map Maker",
+      background: "#afa7d2"},
     "brand": {
       icon: "spa",
-      label: "Heritage Farmed"},
+      label: "Heritage Farmed",
+      background: "#f0e67a"}, //#F8EB5D
     "todo": {
       icon: "clipboard-check",
-      label: "AchieveIt"},
+      label: "AchieveIt",
+      background: "#8ae5ed"}, //#7BF1FC
     "about": {
       icon: "user",
-      label: "About"}
+      label: "About",
+      background: "#cf7cc1"}
   }
 
-  const backgrounds = ["#e7e6f3", "#afa7d2", "#968abf"]
+  // const backgrounds = ["#e7e6f3", "#afa7d2", "#968abf"]
+  // "#00FFDA" - green for achieve it
+  // "#F8EB5D" - yellow for HF
+  // "#968abf" - purple for about + Intro
+  // "#afa7d2" - purple for mapmaker
 
 class NavElem extends React.Component {
   constructor(props) {
@@ -138,6 +149,16 @@ class NavElem extends React.Component {
   }
 }
 
+class PhoneWrapper extends React.Component {
+  render() {
+    return (<div className="fakePhone">
+      <div className="cameraLine"></div>
+      {this.props.screen}
+      <div className="circleBtn"></div>
+    </div>);
+  }
+}
+
 class ImgExpansion extends React.Component {
   constructor(props) {
     super(props);
@@ -163,7 +184,7 @@ class ImgExpansion extends React.Component {
     return (<>
       <div className="imgWrapper" onMouseEnter={this.handleEnter} onMouseLeave={this.handleLeave}>
         {this.state.active &&
-        <FontAwesomeIcon onClick={this.open} icon="expand-alt" size="lg" className={this.props.darkImg ? "light" : "dark"}/>}
+        <FontAwesomeIcon onClick={this.open} icon="expand-alt" size="lg" className={this.props.dark ? "light" : "dark"}/>}
         <img src={this.props.src} alt={this.props.alt}/>
       </div>
       {this.state.open &&
@@ -180,7 +201,7 @@ class SideImg extends React.Component {
   render() {
     return (
       <div className={'sideBySide ' + this.props.side}>
-        <img src={this.props.img.src} alt={this.props.img.alt}/>
+        <ImgExpansion src={this.props.img.src} alt={this.props.img.alt} dark={this.props.img.dark}/>
         <div className='textBox'>
         {this.props.text}
         </div>
@@ -200,7 +221,7 @@ class ImgRow extends React.Component {
     return (
       <div className='imgRow'>
         {this.props.imgs.map(i => {
-          return <img key={i.src} src={i.src} alt={i.alt}/>
+          return <ImgExpansion key={i.src} src={i.src} alt={i.alt}/>
         })}
       </div>
     );
@@ -243,7 +264,7 @@ class Tooltip extends React.Component {
 
   render () {
     let {state} = this;
-    let visiblity = state.visible == true ? "on" : "off";
+    let visiblity = state.visible === true ? "on" : "off";
 
     let style = {
       left: ((state.x + window.scrollX) + 'px'),
@@ -252,7 +273,7 @@ class Tooltip extends React.Component {
 
     let classNames = {tooltip: true};
 
-    if(state.type != null && state.type != "none") {
+    if(state.type !== null && state.type !== "none") {
       classNames[state.type] = true;
     }
 
@@ -271,13 +292,13 @@ class Tooltip extends React.Component {
     let ttNode = ReactDOM.findDOMNode(this);
     if(ttNode != null) {
       let x = 0, y = 0, newState = {};
-      const docWidth = document.documentElement.clientWidth,
-      docHeight = document.documentElement.clientHeight;
+      // const docWidth = document.documentElement.clientWidth,
+      // docHeight = document.documentElement.clientHeight;
 
-      let rx = hoverRect.x + hoverRect.width,
-          lx = hoverRect.x,
-          ty = hoverRect.y,
-          by = hoverRect.y + hoverRect.height;
+      // let rx = hoverRect.x + hoverRect.width,
+      let lx = hoverRect.x,
+          ty = hoverRect.y;
+          // by = hoverRect.y + hoverRect.height;
 
       let ttRect = ttNode.getBoundingClientRect();
 
@@ -325,16 +346,19 @@ class ProjectPage extends React.Component {
 
   setBackground() {
     return {
-      background: backgrounds[this.props.i % backgrounds.length]
+      background: icons[this.props.name].background
     };
   }
 
   render() {
     let image, more;
-    if(this.props.img) {
-      image = <ImgExpansion src={this.props.img.src} alt={this.props.img.alt} darkImg={this.props.img.dark}/>;
+    if (this.props.img) {
+      image = <ImgExpansion src={this.props.img.src} alt={this.props.img.alt} dark={this.props.img.dark}/>;
     }
-    if(this.props.showMore && this.state.showing) {
+    else if (this.props.notImg) {
+      image = this.props.notImg;
+    }
+    if (this.props.showMore && this.state.showing) {
       more = <Content
               parentName = {this.props.name}
               onClick = {this.toggleContent}
@@ -346,9 +370,9 @@ class ProjectPage extends React.Component {
         {image}
         <div className = "text">
           <h2>{this.props.title}</h2>
-          <p>{this.props.intro}</p>
-          {(this.props.showMore && !this.state.showing) ? <a href="#" className="more" onClick={this.toggleContent}>Learn More <FontAwesomeIcon
-          icon="arrow-right"/></a> : ''}
+          {this.props.intro}
+          {(this.props.showMore && !this.state.showing) ? <button className="more" onClick={this.toggleContent}>Learn More <FontAwesomeIcon
+          icon="arrow-right"/></button> : ''}
         </div>
       </div>
       {more}
@@ -367,7 +391,7 @@ class Content extends React.Component {
             className="lower"
           >
             {moreContent[this.props.parentName]}
-            <a href="#" className="more" onClick={this.props.onClick}>Close</a>
+            <button className="more" onClick={this.props.onClick}>Close</button>
           </Element>);
   }
 }
@@ -386,15 +410,17 @@ class Site extends React.Component {
         i={0}
         name="intro"
         showMore={false}
+        reverse={true}
         title="Hi, my name is Emily"
-        intro="I am a double major in computer science and design who just graduated from UC Davis. I am interested in UI/UX design and human computer interaction. Check out my work and get to know me a bit more!"
+        notImg=<img src={portrait} alt="self portrait waving" id="portrait"/>
+        intro=<p>I am a double major in computer science and design who just graduated from UC Davis. I am interested in UI/UX design and human computer interaction. Check out my work and get to know me a bit more!</p>
       />
       <ProjectPage
         i={1}
         name='todo'
-        img={{src: taskPage, alt: "task screen"}}
+        notImg=<PhoneWrapper screen=<img className="screenImg" src={taskPage} alt="task screen"/>/>
         title='AchieveIt'
-        intro='A productivity app designed for people with mental illness. Designed to be gentle, structured, and easy to use. This app can be a helpful tool for people with difficulty remembering tasks and executing them at the right time and place.'
+        intro=<p>A productivity app designed for people with mental illness. Designed to be gentle, structured, and easy to use. This app can be a helpful tool for people with difficulty remembering tasks and executing them at the right time and place.</p>
       />
       <ProjectPage
         i={2}
@@ -402,22 +428,22 @@ class Site extends React.Component {
         reverse={true}
         img={{src: heritageFarmed, alt: 'heritage farmed logo', dark: true}}
         title='Heritage Farmed'
-        intro='Heritage Farmed creates handcrafted botanicals with an emphasis on the community, the environment, and ending the stigma against cannabis. They make medicinal healing products with an emphasis on local, sustainable ingredients. My job was to bring their vision to life and create a full brand material package.'
+        intro=<p>Heritage Farmed creates handcrafted botanicals with an emphasis on the community, the environment, and ending the stigma against cannabis. They make medicinal healing products with an emphasis on local, sustainable ingredients. My job was to bring their vision to life and create a full brand material package.</p>
       />
       <ProjectPage
         i={1}
         name='map'
         img={{src: clickTile, alt: 'map maker screen'}}
         title='Map Maker'
-        intro='MapMaker fills a need in the tabletop RPG community by being an easy to use map creation web app that takes advantage of modern technology to enhance in-person game play.'
+        intro=<p>MapMaker fills a need in the <TooltipBase word='tabletop RPG' def='RPG stands for Role Play Game. Tabletop means the game is played by a group gathering in-person around a table, instead of virtually on a computer.'/> community by being an easy to use map creation web app that takes advantage of modern technology to enhance in-person game play.</p>
       />
       <ProjectPage
         i={2}
         name='about'
         reverse={true}
-        img={{src: profile, alt: 'portrait photo'}}
+        img={{src: headshot, alt: 'portrait photo', dark: true}}
         title="More About Me"
-        intro='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        intro=<p>When I first came to UC Davis, I had a choice between pursuing the world of tech and the world of art. Instead of choosing one or the other, I chose to pursue both. I have a lot of technical aptitude, but I refuse to set my creative abilities aside. By double majoring, I can combine deep technical skills with creative inspiration that can spark new ideas and new ways of doing.</p>
       />
     </>
     );
@@ -436,7 +462,7 @@ let moreContent = {
             alt: "map display screen"
           }]}/>
         <p>
-        The way it works is simple. Game master’s drag and drop tiles to create their map. Once finished, they switch to display mode, which opens a separate window where tiles can be revealed one by one as players explore the map.
+        The way it works is simple. The <TooltipBase word='game master' def='A person who organizes and oversees a role-playing game, in particular by narrating the details of the story that are not controlled by the players.'/> drags and drops tiles to create their map. Once finished, they switch to display mode, which opens a separate window where tiles can be revealed one by one as players explore the map.
         </p>
         <SideImg
           side='left'
@@ -445,12 +471,12 @@ let moreContent = {
         />
         <h2>Knowing the Target Audience</h2>
         <p>
-        In order to best understand the target audience, a survey was sent out to communities of game Masters and RPG game players to collect responses to see what features were most desired. this was collected early on in the process to inform the design of the system and plan for future added features.
+        In order to best understand the target audience, a survey was sent out to communities of game Masters and RPG game players to collect responses to see what features were most desired. This was collected early on in the process to inform the design of the system and plan for future added features.
         </p>
-        <svg id="graph"/>
+        {/*<svg id="graph"/>*/}
         <SideImg
           side='right'
-          img={{src: conferenceDemo, alt: 'conference demo setup'}}
+          img={{src: conferenceDemo, alt: 'conference demo setup', dark: true}}
           text={<>
             <p>
               After that, the system was designed and revised multiple times, where it was presented at Creativity and Cognition 2019. The theme that year was transformational creativity. A paper describing the goals of the project in further detail is published in the <a href='https://dl.acm.org/doi/10.1145/3325480.3326548'>ACM Digital Library</a> and the project itself was demoed during the creative creativity and cognition demonstration event.
@@ -464,13 +490,10 @@ let moreContent = {
   brand: <>
     <h2>It all starts with a logo </h2>
     <p>
-    When I initially met with the heads of heritage farmed, I immediately sensed just how important their ingredients were to their brand. They listed off a flurry of gorgeous botanicals when discussing their products, and I ran with that concept to design a logo system using iconography of these same plants that star in their products.
+    When I initially met with the leaders of heritage farmed, I immediately sensed just how important their ingredients were to their brand. They listed off a flurry of gorgeous botanicals when discussing their products, and I ran with that concept to design a logo system using iconography of these same plants that star in their products.
     </p>
     <ImgRow imgs={[
       {
-      src: altLogos,
-      alt: "black and white logo and dark logo"
-      }, {
         src: logo,
         alt: "primary logo"
       }, {
@@ -494,7 +517,7 @@ let moreContent = {
       <p>
         To really flesh out their brand, a number of additional materials were designed to really give the complete picture of what the heritage farmed brand could look like.
       </p>
-      <p>The full brand standards can be viewed <a href={heritageFarmedBrandStandards} target="_blank">here.</a></p>
+      <p>The full brand standards can be viewed <a href={heritageFarmedBrandStandards} target="_blank" rel="noopener noreferrer">here.</a></p>
       </>,
   todo: <>
     <p>
@@ -510,15 +533,35 @@ let moreContent = {
       User testing and feedback were an integral part of the design process of this project. Potential users in the target demographic were interviewed to get a sense of what they struggled with that a productivity app could help with. An initial version of the app was created for a UC Davis UI/UX class. This version was created with static screens that were mocked in illustrator, and functionality was added through inVision, creating hotspots to allow users to navigate through the mockup. User feedback was collected from 5 participants, which consisted of asking them to complete specific scenarios within the app while they were being observed. Afterward, each participant was asked open-ended questions to collect general feedback on the project.
     </p>
     <div className="flex">
-      <img src = {addTask} alt ="new task screen"/>
-      <img src = {addHabit} alt="new habit screen"/>
-      <img src = {completeHabit} alt="check off habit screen"/>
+      <PhoneWrapper
+        screen=<div className="imgContainer">
+          <img src = {addTask} alt ="new task screen"/>
+        </div>
+      />
+      <PhoneWrapper
+        screen=<div className="imgContainer">
+          <img src = {addHabit} alt="new habit screen"/>
+        </div>
+      />
+      <PhoneWrapper
+        screen=<div className="imgContainer">
+          <img src = {completeHabit} alt="check off habit screen"/>
+        </div>
+      />
     </div>
     <p>
-      This research was used in the next iteration of the project, which continued after the UC Davis class concluded. In this phase, Emily Carlson took over the design of the look and feel of the app, while I implemented the functionality. This version was implemented as a web app, using HTML, CSS, and javascript to implement functionality and design. Steps were taken to integrate the user feedback from the previous version into this one as we worked to improve the design.
+      This research was used in the next iteration of the project, which continued after the UC Davis class concluded. In this phase, a fellow designer took over the design of the look and feel of the app, while I implemented the functionality. This version was implemented as a web app, using HTML, CSS, and javascript to implement functionality and design. Steps were taken to integrate the user feedback from the previous version into this one as we worked to improve the design.
     </p>
     <p>The full project in its current iteration can be viewed <a href="https://github.com/Theacalix/productiveapp/tree/master">here.</a></p>
-      </>
+      </>,
+    about: <>
+      <p>
+        I’ve always been a quick learner and I’ve cultivated that skill while at university. Given any task, I will rise to the challenge, dive right in into figuring out the best way to do it, and get it done. I don’t need my handheld but I’m not afraid to ask for help when I don’t understand something. While employed at Zeeto, I worked with database technology I had never used before, but I took the opportunity to research the specs, experiment with it, and ultimately wrote tools that helped my team get the data they needed. My coworkers appreciated that I didn’t need to be told more than once how to do something and they got more work done when I was on their team.
+      </p>
+      <p>
+        I do great in teams. I can help people hear and understand each other. Because I'm standing at the intersection of tech and design, I can bridge the gap between highly technical individuals and highly creative individuals and “translate” their perspectives from one to the other.
+      </p>
+    </>
 }
 
 
